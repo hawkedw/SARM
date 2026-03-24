6fc1047074acfc634793525f839bc3220fc38698
Dmitriy Allenov <allenov.ds@gmail.com>
2026-03-24 19:38:44 +0300

    fix: add Qt6 to PATH for PyQt6

--- app/run.bat
+++ app/run.bat
@@ -7,7 +7,8 @@ set "ROOT_DIR=%~dp0.."
 set "GIS_DIR=%ROOT_DIR%\gis"
 set "QGIS_DIR=%GIS_DIR%\apps\qgis"
 set "PY_DIR=%GIS_DIR%\apps\Python312"
-set "QT_DIR=%GIS_DIR%\apps\Qt5"
+set "QT5_DIR=%GIS_DIR%\apps\Qt5"
+set "QT6_DIR=%GIS_DIR%\apps\Qt6"
 
 if not exist "%QGIS_DIR%\bin\qgis_core.dll" (
     echo ERROR: QGIS not found at %QGIS_DIR%
@@ -15,13 +16,13 @@ if not exist "%QGIS_DIR%\bin\qgis_core.dll" (
     exit /b 1
 )
 
-set "PATH=%QGIS_DIR%\bin;%GIS_DIR%\bin;%PY_DIR%;%PY_DIR%\Scripts;%QT_DIR%\bin;%PATH%"
+set "PATH=%QGIS_DIR%\bin;%GIS_DIR%\bin;%PY_DIR%;%PY_DIR%\Scripts;%QT6_DIR%\bin;%QT5_DIR%\bin;%PATH%"
 set "PYTHONPATH=%QGIS_DIR%\python;%QGIS_DIR%\python\plugins;%PY_DIR%\Lib\site-packages"
 set "QGIS_PREFIX_PATH=%QGIS_DIR%"
 set "GDAL_DATA=%GIS_DIR%\apps\gdal\share\gdal"
 set "PROJ_LIB=%GIS_DIR%\share\proj"
 set "GDAL_DRIVER_PATH=%GIS_DIR%\apps\gdal\bin\gdalplugins"
-set "QT_PLUGIN_PATH=%QGIS_DIR%\qtplugins;%QT_DIR%\plugins"
+set "QT_PLUGIN_PATH=%QGIS_DIR%\qtplugins;%QT6_DIR%\plugins;%QT5_DIR%\plugins"
 set "PYTHONHOME=%PY_DIR%"
 
 cd /d "%APP_DIR%"
